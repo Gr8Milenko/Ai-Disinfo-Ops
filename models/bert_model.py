9a335b9e66dcf753b910292abf455a796bbbeb14
Gr8Milenko <xtcxgr8milenko@gmail.com>
2025-03-28 16:55:12 -0500

    Create bert_model.py

---
+++ models/bert_model.py
@@ -0,0 +1,12 @@
+class BertDisinfoModel:
+    def __init__(self):
+        # load tokenizer, model weights, etc.
+        pass
+
+    def predict(self, texts):
+        # return ["disinfo", "real", ...]
+        pass
+
+    def predict_proba(self, texts):
+        # return [[0.7, 0.3], [0.2, 0.8], ...]
+        pass
